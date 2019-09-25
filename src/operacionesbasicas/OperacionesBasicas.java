ca512ad2cdf825f60c987ed4f483c142c569a808
Camila <camilalobos.2307@gmail.com>
2019-09-25 15:30:10 -0300

    Se ha comentado el método dividir

--- src/operacionesbasicas/OperacionesBasicas.java
+++ src/operacionesbasicas/OperacionesBasicas.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package operacionesbasicas;
 
-/**
- *
- * @author Lila
- */
 public class OperacionesBasicas {
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        public int sumar(int a, int b){
+        return a+b;
+        }
+        public int restar(int a, int b){
+        return a-b;
+        }
+        public int multiplicar(int a, int b){
+        return a*b;
+        }
+        /*
+        public int dividir(int a, int b){
+        return a/b;
+        }
+        */
+        
+        
+   
+        
+        
     
 }
