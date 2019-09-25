c49e03b8033cdc2f54fe7cfb3d965bedcc50533d
Camila <camilalobos.2307@gmail.com>
2019-09-25 15:44:48 -0300

    Se ha creado nueva clase base de datos

---
+++ src/operacionesbasicas/BaseDeDatos.java
@@ -0,0 +1,23 @@
+package operacionesbasicas;
+
+/**
+ *
+ * @author Lila
+ */
+public class BaseDeDatos {
+
+    public void agregarDatos(Dato d) {
+    }
+
+    public void eliminarDatos() {
+    }
+
+    public void modificarDatos(Dato d) {
+    }
+
+    public void listarDatos() {
+    }
+
+   
+
+}
